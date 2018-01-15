c966689290461905cd0007469590c2dae1c0e0ea
Songkieat Manoharn <m.songkiet@gmail.com>
2018-01-15 09:22:17 +0700

    .

---
+++ geoluxe_defect_logging/Controllers/QC_ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace geoluxe_defect_logging.Controllers
+{
+    public class QC_ReportController : Controller
+    {
+        // GET: QC_Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PM_O()
+        {
+            return View();
+        }
+
+        public ActionResult Shift()
+        {
+            return View();
+        }
+
+        public ActionResult Production()
+        {
+            return View();
+        }
+    }
+}
